c73a2160c643145bda31991b74601e167af812a0
anggalana04 <anggalana03@gmail.com>
2025-04-15 21:27:06 +0700

    add h2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href=" {{ asset('css/style.css') }}">    
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }}">
 </head>
 <body>
     <h1>HALAMAN ABOUT</h1>
+    <h2>haloo dunia</h2>
 </body>
 </html>
