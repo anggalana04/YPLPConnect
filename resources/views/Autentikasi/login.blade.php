c2d6c8b9abd903844440fdcee1c48073f9b64cae
Zal Abdan Hayun <zalabdan029@gmail.com>
2025-05-09 23:33:35 +0700

    add img lokasi di halaman landingpage. membuat halaman login, menambahkan folder css, img untuk halaman login

--- resources/views/Autentikasi/login.blade.php
+++ resources/views/Autentikasi/login.blade.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Page</title>
+
+    <link rel="stylesheet" href="{{ asset('css/Login/login.css') }}">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="">
-        <input type="text">
-    </form>
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="logo-section">
+            <img src="{{ asset('image/logoYPLP/logo.svg') }}" alt="Logo PGRI" class="logo">
+        </div>
+        <div class="login-box">
+            <h1>LOGIN</h1>
+            <form action="proses_login.php" method="POST">
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
