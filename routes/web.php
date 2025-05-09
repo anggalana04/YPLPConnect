0d3fa28099c6ca6586267ba60808d53f3be164de
Zal Abdan Hayun <zalabdan029@gmail.com>
2025-05-09 22:48:05 +0700

    edit .statistik-container

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,6 @@ use App\Http\Controllers\AboutController;
 //route dengan middleware
 
 Route::get('/', function () {
-        return view('v_layouts.index');
+        return view('Landing_Page.landing_page');
     });
 
