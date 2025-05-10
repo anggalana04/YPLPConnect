c6d90dd3a4b4984720b3daa6b36f73a6e22eb22b
Zal Abdan Hayun <zalabdan029@gmail.com>
2025-05-10 23:37:28 +0700

    add file view detail (pada folder pengaduan), add file detail.css (pada folder pengaduan.css)

---
+++ resources/views/v_pengaduan/detail.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset('css/pengaduan/detail-pengaduan.css') }}">
+    <title>Detail Pengaduan</title>
+</head>
+<body>
+    @extends('v_layouts.index')
+
+    <div class="konten">
+        <h1>DETAIL PENGADUAN</h1>
+        <label for="">Status</label>
+
+        <div class="bulat">
+            <div class="bulat1"></div>
+            <div class="bulat2"></div>
+            <div class="bulat3"></div>
+            <div class="bulat4"></div>
+
+            <div class="ket-status">
+                <p>ID Pengaduan :</p>
+                <p>Tanggal Pengaduan :</p>
+            </div>
+        </div>
+
+        <div class="table-box">
+                <table class="table-detail">
+                    <thead>
+                        <tr>
+                            <th>Deskripsi Pengaduan</th>
+                            <th>Bukti Pengaduan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Keluhan tentang pelayanan customer service</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Laporan masalah teknis aplikasi</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+    </div>
+</body>
+</html>
