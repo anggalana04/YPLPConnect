fa6b97eeb0592b10b08d925eb5935592d1136894
anggalana04 <anggalana03@gmail.com>
2025-05-14 21:37:22 +0700

    temporary web

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,25 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/dashboard', function () {
+// Route::get('/dashboard', function () {
+//     return view('operator_yayasan.v_dashboard.index');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+
+
+// });
+
+Route::get('/', function () {
     return view('operator_yayasan.v_dashboard.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-
-    Route::get('/', function () {
-        return view('operator_yayasan.v_dashboard.index');
-    });
-
-    #profile
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    #pengaduan
-    Route::get('/pengaduan', [PengaduanController::class, 'index'])->name('pengaduan.index');
 });
 
-require __DIR__ . '/auth.php';
+#profile
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+#pengaduan
+Route::get('/pengaduan', [PengaduanController::class, 'index'])->name('pengaduan.index');
+
+// require __DIR__ . '/auth.php';
