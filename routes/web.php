5bb923a33372db005707415c09302e3ab641b9e1
Zal Abdan Hayun <zalabdan029@gmail.com>
2025-05-14 23:04:38 +0700

    no edit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/dashboard', function () {
     return view('operator_yayasan.v_dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
